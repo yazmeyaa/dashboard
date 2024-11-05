49588f2f12377c459fdb22b4c5d8f6947a63dcbf
Eugene <snowflake4@yandex.ru>
2024-11-05 23:05:27 +0300

    add current path in navigation highlight

--- src/lib/components/NavigationItem/NavigationItem.svelte
+++ src/lib/components/NavigationItem/NavigationItem.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import { page } from '$app/stores';
 
 	interface Props {
 		href: string;
 		children: Snippet;
 	}
 	const { href, children }: Props = $props();
+
+	let active = $derived.by(() => {
+		return $page.url.pathname.startsWith(href);
+	});
 </script>
 
-<li class="text-neutral-50 transition-colors hover:bg-neutral-600 rounded-lg">
-	<a {href} class="block px-2 py-4 text-xl">
+<li
+	class:bg-neutral-800={active}
+	class="rounded-lg text-neutral-50 transition-colors hover:bg-neutral-600"
+>
+	<a {href} class:underline={active} class="block px-2 py-4 text-xl">
 		<span>{@render children()}</span>
 	</a>
 </li>
